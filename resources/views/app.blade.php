dc81fe0bdbc11297ddc6b77bb7f6f3be56667396
Paulius Savičiūnas <psaviciunas@gmail.com>
2026-03-06 13:04:43 +0200

    fixed initial commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<<html>
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @viteReactRefresh
-        @vite(['resources/js/app.js', 'resources/css/app.css'])
+        @vite(['resources/js/app.jsx', 'resources/css/app.css'])
         @inertiaHead
     </head>
     <body>
