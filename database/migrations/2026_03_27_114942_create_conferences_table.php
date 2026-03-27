c045d8f15ccc11c5b25beb2a70ba4dee624b6e7a
Paulius Savičiūnas <psaviciunas@gmail.com>
2026-03-27 14:04:34 +0200

    added conf table

--- database/migrations/2026_03_27_114942_create_conferences_table.php
+++ database/migrations/2026_03_27_114942_create_conferences_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,12 @@ return new class extends Migration
         Schema::create('conferences', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('date');
+            $table->string('location');
+            $table->enum('status', array_column(Status::cases(), 'value'))->default(Status::PLANNED->value);
+            $table->string('lectors');
         });
     }
 
